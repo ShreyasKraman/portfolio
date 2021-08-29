e818a583135893087555723f6716d0744a81b17f
Shreyas <shreyas.kalyanaraman@gmail.com>
2021-08-29 12:37:39 -0400

    Rename filenames

--- src/container/Portfolio/Portfolio.tsx
+++ src/container/Portfolio/Portfolio.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Introduction from '../../components/Sections/Introduction/Introduction';
 import AboutMe from '../../components/Sections/AboutMe/AboutMe';
 import Timeline from '../../components/Sections/Timeline/timeline';
-import Contact from '../../components/Sections/Contact/contact';
+import Contact from '../../components/Sections/Contact/Contact';
 
 import './Portfolio.scss';
 
