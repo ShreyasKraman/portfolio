5008c31244251b9f3640c977a59bdbed3f812873
Shreyas <shreyas.kalyanaraman@gmail.com>
2021-09-04 17:00:47 -0400

    Fix file path

--- tests/Container/Portfolio.test.js
+++ tests/Container/Portfolio.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {render} from '@testing-library/react'
-import Portfolio from '../src/container/Portfolio/Portfolio'; 
+import Portfolio from '../../src/container/Portfolio/Portfolio'; 
 
 
 describe('Porfolio component tests',() => {
